cdb7afa1bb771369ae79fdd2cbaa751933645b33
Abner <abneraq73@gmail.com>
2021-09-22 13:36:09 -0600

    Divided projects of the server and client app

---
+++ ServerProject/cmake-build-debug/CMakeFiles/ServerProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ServerProject.dir/main.cpp.o"
+  "ServerProject"
+  "ServerProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ServerProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
